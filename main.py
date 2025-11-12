debf0ce295a3a0205aa366c8e9ab8f11fe7b4ab2
AdamMokrzycki <92626198+AdamM002@users.noreply.github.com>
2025-11-12 15:55:36 +0100

    Poprawki Eli do wyglądu menu i działania przechodzenia między oknami

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QLabel
 from PySide6.QtGui import QPixmap
-from menu import MenuWindow
+from menu import MainWindow
 import sys
 
 def main():
     app = QApplication(sys.argv)
-    window = MenuWindow()
+    window = MainWindow()
     window.show()
     app.exec()
 
