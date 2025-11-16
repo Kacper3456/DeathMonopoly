6dbcccb6610ec8d2e32033161fbf611955a191b3
larslars19 <151089854+larslars19@users.noreply.github.com>
2025-11-16 22:25:17 +0100

    dodano class Player, gdzie będą przechowywane dane o graczu (nas)

---
+++ player_manager.py
@@ -0,0 +1,27 @@
+class PlayerManager:
+    def __init__(self):
+        self.player_data = {
+            "name": "You",
+            "avatar": "images/game_window/avatar/businessman.png",
+            "dialogue": "This is your character. You are the player in this game."
+        }
+    
+    def get_player_data(self):
+        return self.player_data
+    
+    def set_player_name(self, name):
+        self.player_data["name"] = name
+    
+    def set_player_avatar(self, avatar_path):
+        self.player_data["avatar"] = avatar_path
+    
+    def set_player_dialogue(self, dialogue):
+        self.player_data["dialogue"] = dialogue
+    
+    def update_player_data(self, name=None, avatar=None, dialogue=None):
+        if name is not None:
+            self.player_data["name"] = name
+        if avatar is not None:
+            self.player_data["avatar"] = avatar
+        if dialogue is not None:
+            self.player_data["dialogue"] = dialogue
